e1ec105e43697cb602ab77f80c1de55de82cc7a5
Nick Mitin <nick.mitin@gmail.com>
2023-10-30 13:36:06 +0300

    Added static builds

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,15 @@ COPY ./package.json ./
 RUN yarn install && yarn cache clean --force
 ENV PATH=/app/node_modules/.bin:$PATH
 ENV VUE_APP_API_BASE_URL=https://$DOMAIN_NAME
-ENV VUE_APP_API_BASE_URL=https://tiger.prod.qortex.ru
-ENV VITE_APP_API_BASE_URL=https://tiger.prod.qortex.ru
-ENV VITE_JSON_API_BASE_URL=https://tiger.prod.qortex.ru/
+ENV VUE_APP_API_BASE_URL=https://%%api-base-host%%
+ENV VITE_APP_API_BASE_URL=https://%%api-base-host%%
+ENV VITE_JSON_API_BASE_URL=https://%%api-base-host%%/
 ENV VITE_API_KEY=k9M3ghBpyFU8DszpfnsrVz3ASTsRESYP
+COPY ./tools/imagemin.js ./tools/imagemin.js
+COPY ./tools/imagemin-dev.js ./tools/imagemin-dev.js
 # Will be overwritten with the contents of src folder when running via docker-compose-dev.yml
 WORKDIR /app/src
 COPY . .
 RUN yarn build
+RUN node /app/tools/imagemin.js
 CMD [ "yarn", "dev" ]
---
+++ tools/imagemin-dev.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const path = require('path')
+const imagemin = require('imagemin')
+const imageminJpegtran = require('imagemin-jpegtran')
+const imageminPngquant = require('imagemin-pngquant')
+
+const bytesToMegabytes = bytes => Math.round((bytes / (1024 * 1024)  + Number.EPSILON) * 100) / 100
+
+const getAllFiles = function (dirPath, arrayOfFiles) {
+  files = fs.readdirSync(dirPath)
+
+  arrayOfFiles = arrayOfFiles || []
+
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + '/' + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(path.join(dirPath, '/', file))
+    }
+  })
+
+  return arrayOfFiles
+}
+;(async () => {
+  const sourceFiles = getAllFiles('/app/src/assets')
+  for (const sourceFile of sourceFiles) {
+    console.log(sourceFile)
+    const fileName = path.basename(sourceFile)
+    const filePath = path.dirname(sourceFile)
+    const originalFileSize = bytesToMegabytes(fs.statSync(sourceFile).size)
+    const files = await imagemin([sourceFile], {
+      destination: filePath,
+      plugins: [
+        imageminJpegtran(),
+        imageminPngquant({
+          quality: [0.6, 0.8]
+        })
+      ]
+    })
+    const resultFileSize = bytesToMegabytes(fs.statSync(sourceFile).size)
+    console.log(fileName, originalFileSize, resultFileSize)
+  }
+})()
